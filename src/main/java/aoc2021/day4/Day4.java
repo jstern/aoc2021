a0c5fc643197e43ad0794a6eeac7d47f1fed0751
Jon Stern <jonathan.a.stern@gmail.com>
2021-12-04 09:21:10 -0600

    started day 4

---
+++ src/main/java/aoc2021/day4/Day4.java
@@ -0,0 +1,13 @@
+package aoc2021.day4;
+
+import aoc2021.Solution;
+
+public final class Day4 implements Solution {
+    public final Object part1(String input) {
+        return "";
+    }
+
+    public final Object part2(String input) {
+        return "";
+    }
+}
