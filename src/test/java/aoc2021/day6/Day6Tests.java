0d203b99657d835b905cfb9857acb4b1fb9ef3e6
Jon Stern <jonathan.a.stern@gmail.com>
2021-12-06 07:26:54 -0600

    started day 6

---
+++ src/test/java/aoc2021/day6/Day6Tests.java
@@ -0,0 +1,17 @@
+package aoc2021.day6;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day6Tests {
+
+    @Test
+    @DisplayName("day 6 something does something")
+    @Disabled
+    void something() {
+        assertEquals(true, false);
+    }
+}
