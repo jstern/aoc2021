04f3936a18d811594d0449fed4c6df461e3ec846
Jon Stern <jonathan.a.stern@gmail.com>
2021-12-07 06:25:49 -0600

    started day 7

---
+++ src/test/java/aoc2021/day7/Day7Tests.java
@@ -0,0 +1,27 @@
+package aoc2021.day7;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day7Tests {
+
+    static String INPUT = "";
+
+    @Test
+    @DisplayName("day 7 part 1 example")
+    @Disabled
+    void day7part1() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("day 7 part 2 example")
+    @Disabled
+    void day7part2() {
+        assertEquals(true, false);
+    }
+
+}
