02aceacee98249d92742f0515c321995b5ded761
Jon Stern <jonathan.a.stern@gmail.com>
2021-12-12 14:47:30 -0600

    started day 12

---
+++ src/main/java/aoc2021/day12/Day12.java
@@ -0,0 +1,13 @@
+package aoc2021.day12;
+
+import aoc2021.Solution;
+
+public final class Day12 implements Solution {
+    public Object part1(String input) {
+        return "";
+    }
+
+    public Object part2(String input) {
+        return "";
+    }
+}
---
+++ src/test/java/aoc2021/day12/Day12Tests.java
@@ -0,0 +1,27 @@
+package aoc2021.day12;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day12Tests {
+
+    static String INPUT = "";
+
+    @Test
+    @DisplayName("day 12 part 1 example")
+    @Disabled
+    void day12part1() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("day 12 part 2 example")
+    @Disabled
+    void day12part2() {
+        assertEquals(true, false);
+    }
+
+}
