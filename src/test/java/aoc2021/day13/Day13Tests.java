e27f5fca062188cca78c1d71ccbb0a8b2a3294d9
Jon Stern <jonathan.a.stern@gmail.com>
2021-12-18 16:57:33 -0600

    started day 13

---
+++ src/test/java/aoc2021/day13/Day13Tests.java
@@ -0,0 +1,27 @@
+package aoc2021.day13;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day13Tests {
+
+    static String INPUT = "";
+
+    @Test
+    @DisplayName("day 13 part 1 example")
+    @Disabled
+    void day13part1() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("day 13 part 2 example")
+    @Disabled
+    void day13part2() {
+        assertEquals(true, false);
+    }
+
+}
