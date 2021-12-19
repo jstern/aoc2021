40daddd7194ec657751ef002d38e89b60aeb297c
Jon Stern <jonathan.a.stern@gmail.com>
2021-12-19 14:18:30 -0600

    started day 14

---
+++ src/main/java/aoc2021/day14/Day14.java
@@ -0,0 +1,13 @@
+package aoc2021.day14;
+
+import aoc2021.Solution;
+
+public final class Day14 implements Solution {
+    public Object part1(String input) {
+        return "";
+    }
+
+    public Object part2(String input) {
+        return "";
+    }
+}
---
+++ src/test/java/aoc2021/day14/Day14Tests.java
@@ -0,0 +1,27 @@
+package aoc2021.day14;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day14Tests {
+
+    static String INPUT = "";
+
+    @Test
+    @DisplayName("day 14 part 1 example")
+    @Disabled
+    void day14part1() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("day 14 part 2 example")
+    @Disabled
+    void day14part2() {
+        assertEquals(true, false);
+    }
+
+}
