b7b702435451686c5953ccc2cc6016018484ea04
Jon Stern <jonathan.a.stern@gmail.com>
2022-01-09 11:21:43 -0600

    started day 18

---
+++ src/test/java/aoc2021/day18/Day18Tests.java
@@ -0,0 +1,27 @@
+package aoc2021.day18;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day18Tests {
+
+    static String INPUT = "";
+
+    @Test
+    @DisplayName("day 18 part 1 example")
+    @Disabled
+    void day18part1() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("day 18 part 2 example")
+    @Disabled
+    void day18part2() {
+        assertEquals(true, false);
+    }
+
+}
