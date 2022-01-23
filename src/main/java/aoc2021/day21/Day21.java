4c7a647b3755a7ca018af1bf056674f69ff6e1e4
Jon Stern <jonathan.a.stern@gmail.com>
2022-01-23 11:45:00 -0600

    started day 21

---
+++ src/main/java/aoc2021/day21/Day21.java
@@ -0,0 +1,13 @@
+package aoc2021.day21;
+
+import aoc2021.Solution;
+
+public final class Day21 implements Solution {
+    public Object part1(String input) {
+        return "";
+    }
+
+    public Object part2(String input) {
+        return "";
+    }
+}
