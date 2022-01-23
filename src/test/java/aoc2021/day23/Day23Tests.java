a6e48487a80138088d3f5b75123048adda2f78ed
Jon Stern <jonathan.a.stern@gmail.com>
2022-01-23 11:45:07 -0600

    started day 23

---
+++ src/test/java/aoc2021/day23/Day23Tests.java
@@ -0,0 +1,27 @@
+package aoc2021.day23;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day23Tests {
+
+    static String INPUT = "";
+
+    @Test
+    @DisplayName("day 23 part 1 example")
+    @Disabled
+    void day23part1() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("day 23 part 2 example")
+    @Disabled
+    void day23part2() {
+        assertEquals(true, false);
+    }
+
+}
