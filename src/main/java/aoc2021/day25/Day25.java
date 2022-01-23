a3db2780b23f0148c37a4f2980c7694f3068b163
Jon Stern <jonathan.a.stern@gmail.com>
2022-01-23 11:45:14 -0600

    started day 25

---
+++ src/main/java/aoc2021/day25/Day25.java
@@ -0,0 +1,13 @@
+package aoc2021.day25;
+
+import aoc2021.Solution;
+
+public final class Day25 implements Solution {
+    public Object part1(String input) {
+        return "";
+    }
+
+    public Object part2(String input) {
+        return "";
+    }
+}
---
+++ src/test/java/aoc2021/day25/Day25Tests.java
@@ -0,0 +1,27 @@
+package aoc2021.day25;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day25Tests {
+
+    static String INPUT = "";
+
+    @Test
+    @DisplayName("day 25 part 1 example")
+    @Disabled
+    void day25part1() {
+        assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("day 25 part 2 example")
+    @Disabled
+    void day25part2() {
+        assertEquals(true, false);
+    }
+
+}
